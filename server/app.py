0ccc4d8ff49aca188cdf9bee728060fe52a662af
Timothy <sanohoang@gmail.com>
2023-09-07 14:12:34 -0500

    created models table

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
+from models import User
 from config import api, app, db, abort
 from flask_restful import Resource
 from flask import request, make_response, session
+
+
+class Users(Resource):
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(users, 200)
+
+
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/config.py
+++ server/config.py
@@ -5,16 +5,21 @@ from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/build',
-    template_folder='../client/build'
-)
+# app = Flask(
+#     __name__,
+#     static_url_path='',
+#     static_folder='../client/build',
+#     template_folder='../client/build'
+# )
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 db = SQLAlchemy()
 
