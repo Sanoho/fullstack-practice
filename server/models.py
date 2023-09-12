a0393e4816f95649d8489d1afbd10bb73450ac2c
Timothy <sanohoang@gmail.com>
2023-09-12 09:17:33 -0500

    updating table

--- server/models.py
+++ server/models.py
@@ -8,8 +8,40 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
+    serialize_rules = ('-created_at', '-updated_at',
+                       '-_password_hash',)
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
-    picture= db.Column(db.String, nullable=False, unique=True)
-    password=db.Column(db.String, nullable=False, unique=False)
+    picture = db.Column(db.String, nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, value):
+        if not value and 1 <= value <= 20:
+            raise ValueError(
+                'Must have a username between 1 and 20 characters')
+        return value
+
+    @validates('email')
+    def validate_email(self, key, value):
+        if not value:
+            raise ValueError('Must have an email address')
+        return value
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-function Login(){
-    return(
-        <div>
-            <form className="loginForm">Login Username
-                <input style={{ width: '300px',height:'30px'}}></input>
-                <input style={{ width: '300px',height:'30px'}}></input>
-                <button style={{ width: '100px',height:'30px'}}>Submit</button>
-            </form>
-        </div>
-    )
+function Login() {
+  return (
+    <div>
+      <form className="loginForm">
+        Login Username
+        <input style={{ width: "300px", height: "30px" }}></input>
+        <input style={{ width: "300px", height: "30px" }}></input>
+        <button style={{ width: "100px", height: "30px" }}>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
